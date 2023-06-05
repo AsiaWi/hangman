5fa79395e68ccd0fdab62c00dde36fe9af1936c9
AsiaWi <joannawitek@icloud.com>
2023-06-05 22:42:32 +0000

    Initial commit, import a list of words for the game

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from english_words import get_english_words_set
+web2lowerset = get_english_words_set(['web2'])
+
+print(web2lowerset)
