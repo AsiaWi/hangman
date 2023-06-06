ad1de8c5406dab70174f2aa2dd8f0732e0b07054
AsiaWi <joannawitek@icloud.com>
2023-06-06 17:48:16 +0000

    Add print selected word function as blank underscore

---
+++ hangman.py
@@ -0,0 +1,26 @@
+
+def print_hangman(values):
+    print()
+    print("\t +--------+")
+    print("\t |       | |")
+    print("\t {}       | |".format(values[0]))
+    print("\t{}{}{}      | |".format(values[1], values[2], values[3]))
+    print("\t {}       | |".format(values[4]))
+    print("\t{} {}      | |".format(values[5],values[6]))
+    print("\t         | |")
+    print("  _______________|_|___")
+    print("  `````````````````````")
+    print()
+
+    def print_hangman_win():
+    print()
+    print("\t +--------+")
+    print("\t         | |")
+ 
+    print("\t         | |")
+    print("\t O       | |")
+    print("\t/|\\      | |")
+    print("\t |       | |")
+    print("  ______/_\\______|_|___")
+    print("  `````````````````````")
+    print()
--- run.py
+++ run.py
@@ -1,22 +1,26 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-import random
+import random, hangman
 from english_words import get_english_words_set
 web2set = get_english_words_set(['web2'])
+
 
 def pick_random_word(web2set):
     word = random.choice(web2set).upper()
     while len(word) < 6:
         word = random.choice(web2set).upper()
+    return word
 
-
-
-    print(word) 
+def print_randomly_selected_word(word):
+    print('_'* len(word))
+    
     
 def main():
+    print('Welcome to Hangman game! Guess the word and save his life:)')
     word_list= list(web2set)
-    pick_random_word(word_list)
-    
+    word=pick_random_word(word_list)
+    print_randomly_selected_word(word)
+   
     
 main()
