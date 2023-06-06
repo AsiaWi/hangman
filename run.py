9063e4ca0d373ebf8c8e08264bd18138b8b3f0a8
AsiaWi <joannawitek@icloud.com>
2023-06-06 01:12:41 +0000

    Add function to select random word from the list

--- run.py
+++ run.py
@@ -1,7 +1,22 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
 from english_words import get_english_words_set
-web2lowerset = get_english_words_set(['web2'])
+web2set = get_english_words_set(['web2'])
 
-print(web2lowerset)
+def pick_random_word(web2set):
+    word = random.choice(web2set).upper()
+    while len(word) < 6:
+        word = random.choice(web2set).upper()
+
+
+
+    print(word) 
+    
+def main():
+    word_list= list(web2set)
+    pick_random_word(word_list)
+    
+    
+main()
